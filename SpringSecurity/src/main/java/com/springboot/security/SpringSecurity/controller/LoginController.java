49f11b29e198b6640956293afaf02058bc24ad3f
mohmmad1144 <mailtomohmmad@gmail.com>
2019-04-15 01:26:54 +0200

    logout controller has been modified

--- SpringSecurity/src/main/java/com/springboot/security/SpringSecurity/controller/LoginController.java
+++ SpringSecurity/src/main/java/com/springboot/security/SpringSecurity/controller/LoginController.java
@@ -1,8 +1,13 @@
 package com.springboot.security.SpringSecurity.controller;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,26 +17,32 @@ public class LoginController {
 	
 	@Autowired 
 	private ServletContext context;
+
+	
 	
 	@GetMapping(value="/")
 	public String login() {
 		return "redirect:/login";
 	}
 	
+	
+	
+	
 	@GetMapping(value="/login")
 	public String loginpage() {
 		return "view/login";
-		
 	}
+	
+	
 	
 	@GetMapping(value="/loginfailure")
 	public String loginfailure(Model model) {
 		String email = (String)context.getAttribute("email");
-		String password = (String)context.getAttribute("password");
-		System.out.println(email+ " "+password);
+		System.out.println(email + "  & its password  is Invalid  ");
 		model.addAttribute("errormessage", "Invalid Email & Password");
 		return "view/login";
 	}
+	
 	
 	@GetMapping(value="/access-denied")
 	public String accessdenied(Model model) {
@@ -41,10 +52,13 @@ public class LoginController {
 	
 	
 	@GetMapping(value="/logout")
-	public String logout(Model model) {
-		model.addAttribute("successmessage", "Logout Successfully");
-		return "view/login";
-	}
+	public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+	    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    if (auth != null){    
+	        new SecurityContextLogoutHandler().logout(request, response, auth);
+	    }
+	    return "view/login";	
+	    }
 	
 	
 
